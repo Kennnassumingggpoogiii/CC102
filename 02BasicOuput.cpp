e9863e3e0a2380680e19b98285328af1e732629d
Kennnassumingggpoogiii <kennfernandez41@gmail.com>
2024-10-03 12:28:02 +0800

    Add files via upload

---
+++ 02BasicOuput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	 cout << "Our Lady Of Lourdes College Foundation" << endl;
+	 cout << "Computer Science Technology" << endl;
+	 cout << "Bachelor Science Information Technology" << endl;
+	 cout << "Kenn Fernandez" << endl;
+	 cout << "P-4 Brgy.Calabagas San Vicente Camarines Norte" << endl;
+	 return 0;
+	 
+}
