2cfa726e888d708cf4c566123d6ccd5c06a007c7
Kennnassumingggpoogiii <kennfernandez41@gmail.com>
2024-10-03 13:23:55 +0800

    Add files via upload

---
+++ 02Profile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Name\t\t\t:\tKenn A. Fernandez"<< endl;
+	cout << "Course/YrLvl/Section\t:\tBSIT/1st year Block-A" << endl;
+	cout << "Address\t\t\t:\tBrgy.Calabgas San Vicente Cmamrines Norte" << endl;
+	cout << "*****************************************" << endl;
+	cout << "YES! PROGRAMMER NA AKO GUYS! <3 Uwu!" << endl;
+	cout << "*****************************************" << endl;
+	cout << "Favorite\t\t:\twhite" << endl;
+	
+	return 0;
+}
