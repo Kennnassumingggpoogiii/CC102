44d25eeac79746488439e457aa568a572e5d54ed
Kennnassumingggpoogiii <kennfernandez41@gmail.com>
2024-10-03 13:12:11 +0800

    Add files via upload

---
+++ 04Input.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string name;
+	int age;
+	
+	cout << "---------------------------------------------" << endl;
+	cout << "Enter the name of the student\t|\t";
+	cin >> name;
+	cout << "Age\t\t\t\t|\t";
+	cin >> age;
+	cout << "---------------------------------------------" << endl;
+	cout << "Howdy " << "Your age is " << age << " and for me you are too young!" << endl;
+	cout << "---------------------------------------------" << endl;
+	return 0;
+	
+}
