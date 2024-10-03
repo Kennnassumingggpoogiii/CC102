044c12b9eb56313ed4577788b92f3507a45a86d0
Kennnassumingggpoogiii <kennfernandez41@gmail.com>
2024-10-03 12:47:07 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	string name, CYL, school;
+	cout << "Enter you name: ";
+	cin >> name;
+	cout << "Course-Year Level: ";
+	cin >> CYL;
+	cout << "School: ";
+	cin >> school;
+	
+	return 0;
+}
