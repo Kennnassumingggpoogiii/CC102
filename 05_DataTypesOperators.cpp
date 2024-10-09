a87acb67847b9e3967a3291750965e423e9c4b47
Kennnassumingggpoogiii <kennfernandez41@gmail.com>
2024-10-09 09:38:53 +0800

    Add files via upload

---
+++ 05_DataTypesOperators.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,b;
+	
+	cout << "Enter First Number" << endl;
+	cin >> a;
+	
+	cout << "Enter Second Number" << endl;
+	cin >> b;
+	
+	
+	cout << "sum: " << a << '+' << b << '=' << a+b << endl;
+	
+	cout << "Difference: " << a << '-' << b << '=' << a-b << endl;
+	
+	cout << "Quotient: " << a << '/' << b << '=' << a/b << endl;
+	
+	cout << "Product: " << a << '*' << b << '=' << a*b << endl;
+	
+	cout << "Remainder: " << a << '%' << b << '=' << a%b << endl;
+	
+	
+	
+	return 0;
+
+}
